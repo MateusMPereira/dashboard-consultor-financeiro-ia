03f18a55a8e616d1290673eac398a79e0cb43804
Mateus Pereira <mateusmichellipereira@gmail.com>
2025-10-21 11:46:25 -0300

    Add Sidebar component with navigation links

---
+++ src/components/Sidebar.js
@@ -0,0 +1,12 @@
+export default function Sidebar() {
+return (
+<aside className="w-64 p-4 border-r hidden md:block">
+<ul className="space-y-2 text-sm">
+<li className="font-medium">Visão geral</li>
+<li>Relatórios</li>
+<li>Usuários</li>
+<li>Configurações</li>
+</ul>
+</aside>
+)
+}
