380e28fb9cca8a45a37bbb73b29516fe6ee9514c
Mateus Pereira <mateusmichellipereira@gmail.com>
2025-11-19 11:52:54 -0300

    Update start.sh

--- server/start.sh
+++ server/start.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 set -e
 
-# Start nginx in background (daemon)
-nginx || true
-
 # Export Chromium path if installed
 if [ -x "/usr/bin/chromium" ]; then
 	export PUPPETEER_EXECUTABLE_PATH=/usr/bin/chromium
 fi
 
-# Start node server (runs in foreground)
-exec node /app/server/index.js
+# Start node server in background so nginx can remain PID 1
+node /app/server/index.js &
+
+# Start nginx in foreground (PID 1)
+nginx -g 'daemon off;'
